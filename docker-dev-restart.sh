f2e8ed2a4e112a81ad0afb463096633e6d767d3c
ouyafei <ouyafei@pxsj.com>
2016-12-15 18:01:59 +0800

    add stomp and activemq support

--- docker-dev-restart.sh
+++ docker-dev-restart.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+eval "$(docker-machine env default)"
+
 cd ~/dev/laradock-chinese
 #docker-compose build --no-cache mysql redis nginx
 docker-compose down
-docker-compose build php-fpm workspace mysql nginx redis
-docker-compose up -d mysql redis nginx phpmyadmin
+# docker-compose build php-fpm workspace mysql nginx redis elasticsearch beanstalkd-console beanstalkd
+docker-compose build php-fpm workspace mysql nginx redis elasticsearch
+docker-compose up -d mysql redis nginx phpmyadmin elasticsearch
 docker ps -a
--- docker-dev.sh
+++ docker-dev.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+
+#export DOCKER_TLS_VERIFY="1"
+#export DOCKER_HOST="tcp://192.168.99.100:2376"
+#export DOCKER_CERT_PATH="C:\Users\yafei\.docker\machine\machines\default"
+#export DOCKER_MACHINE_NAME="default"
+
+
+#docker-machine env default
+eval "$(docker-machine env default)"
+#docker info
+
 cd ~/dev/laradock-chinese
 #docker-compose build --no-cache mysql redis nginx
-docker-compose build mysql redis nginx php-fpm workspace
-docker-compose up -d mysql redis nginx
+docker-compose build mysql redis nginx php-fpm workspace elasticsearch
+docker-compose up -d mysql redis nginx elasticsearch
