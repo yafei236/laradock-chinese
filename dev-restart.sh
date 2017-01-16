89069a3cbfb9847ed3e332fa1ab7791d14073fac
ouyafei <ouyafei@pxsj.com>
2017-01-16 13:22:02 +0800

    add uploads support

--- dev-restart.sh
+++ dev-restart.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 eval "$(docker-machine env default)"
+# cd ~/dev/laradock-chinese
 
-cd ~/dev/laradock-chinese
 #docker-compose build --no-cache mysql redis nginx
-docker-compose down
+
 # docker-compose build php-fpm workspace mysql nginx redis elasticsearch beanstalkd-console beanstalkd
+
+# docker-compose build --no-cache php-fpm workspace mysql nginx redis elasticsearch phpmyadmin
+
+docker-compose down
 docker-compose build php-fpm workspace mysql nginx redis elasticsearch phpmyadmin
 docker-compose up -d mysql redis nginx phpmyadmin elasticsearch
 docker ps -a
